04de0d75407cade89f5563b71b2273e150fe3c1f
MR AS GORDON <inforecruitmyenglish@gmail.com>
2026-06-24 19:14:13 +0000

    Add rme-hud: always-on top-right info panel (ID, CID, cash, bank, job, gang)

---
+++ resources/[qb]/rme-hud/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'RME'
+description 'RME always-on info HUD (Server ID, Cash, Bank, Job, Gang)'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'client.lua'
+}
+
+files {
+    'html/index.html'
+}
+
+dependency 'qb-core'
