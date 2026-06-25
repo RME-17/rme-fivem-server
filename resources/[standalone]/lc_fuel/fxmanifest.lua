f7f6336cd87254bafe8b8eef7bd9a4a2fbb4261f
MR AS GORDON <inforecruitmyenglish@gmail.com>
2026-06-25 22:55:13 +0200

    Add lc_utils + lc_fuel (LC Fuel system)

---
+++ resources/[standalone]/lc_fuel/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'LixeiroCharmoso'
+name 'lc_fuel'
+
+-- The download link and install instructions can be found on our docs:
+-- https://docs.lixeirocharmoso.com/lc_fuel/installation
+-- Thanks :)
+dependency '/assetpacks'
