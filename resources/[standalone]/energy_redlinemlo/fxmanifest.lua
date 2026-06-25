de607bdf8ef930a7ea376738356afeedbd2ee928
MR AS GORDON <inforecruitmyenglish@gmail.com>
2026-06-25 20:10:38 +0200

    Add Redline mechanic MLO

---
+++ resources/[standalone]/energy_redlinemlo/fxmanifest.lua
@@ -0,0 +1,19 @@
+
+
+fx_version "bodacious"
+game "gta5"
+author "Energy Shop - https://discord.gg/MwyyXSwdC9"
+
+this_is_a_map "yes"
+
+lua54 'yes'
+
+
+escrow_ignore {
+
+    'stream/editable/*.ydr'
+} 
+
+
+
+dependency '/assetpacks'
