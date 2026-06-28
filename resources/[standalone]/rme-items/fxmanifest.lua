45bf1fdab8d15394f8953e17fa4b5b44bfd99013
MR AS GORDON <inforecruitmyenglish@gmail.com>
2026-06-28 18:40:03 +0200

    Add rme-items resource (registers custom diamond_chain item)

---
+++ resources/[standalone]/rme-items/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'RME'
+description 'Registers custom RME items at runtime via the qb-core AddItem export (avoids editing qb-core/shared/items.lua directly)'
+version '1.0.0'
+
+server_scripts {
+    'server.lua'
+}
