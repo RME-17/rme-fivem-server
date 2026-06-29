d659dd68ddd379edfbc94bb10e4ca4e197eeb02a
MR AS GORDON <inforecruitmyenglish@gmail.com>
2026-06-30 01:24:35 +0200

    rme-speedo: redesign speedometer HUD (glass gauge, gradient glow, ticks) + cache-bust to hud2.html

--- resources/[qb]/rme-speedo/fxmanifest.lua
+++ resources/[qb]/rme-speedo/fxmanifest.lua
@@ -3,11 +3,11 @@ game 'gta5'
 
 author 'RME'
 description 'RME dedicated speedometer (cars + aircraft) + square minimap reposition'
-version '1.2.0'
+version '1.3.0'
 
--- UI page filename intentionally bumped from index.html -> hud.html to force
--- the client NUI/CEF to fetch a fresh page (the old filename was being cached).
-ui_page 'html/hud.html'
+-- UI page filename intentionally bumped (index.html -> hud.html -> hud2.html) to
+-- force the client NUI/CEF to fetch a fresh page (old filenames get cached).
+ui_page 'html/hud2.html'
 
 client_scripts {
     'client.lua',
@@ -15,5 +15,5 @@ client_scripts {
 }
 
 files {
-    'html/hud.html'
+    'html/hud2.html'
 }
