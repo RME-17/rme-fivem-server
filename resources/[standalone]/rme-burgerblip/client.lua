f7e6b2e80ab6aa0894362608780d8507b7cea1f3
MR AS GORDON <inforecruitmyenglish@gmail.com>
2026-06-30 13:23:09 +0200

    Speedo v1.7.0 boxless HUD (remove panel, bigger speed, smaller icons) + add rme-burgerblip (Little Seoul)

---
+++ resources/[standalone]/rme-burgerblip/client.lua
@@ -0,0 +1,20 @@
+-- Map blip for Giant Burger / Burger Shot MLO (Little Seoul, Giant_Burger resource)
+-- Adjust SPRITE / COLOUR / SCALE / LABEL below to taste.
+
+local BURGER_COORDS = vector3(-595.71, -861.46, 25.89)
+local SPRITE = 889      -- food/diner-style icon (GTA V has no native burger icon). Swap this number for a different icon.
+local COLOUR = 17       -- 17 = orange (burger branding)
+local SCALE  = 0.9
+local LABEL  = 'Burger Shot'
+
+CreateThread(function()
+    local blip = AddBlipForCoord(BURGER_COORDS.x, BURGER_COORDS.y, BURGER_COORDS.z)
+    SetBlipSprite(blip, SPRITE)
+    SetBlipDisplay(blip, 4)
+    SetBlipScale(blip, SCALE)
+    SetBlipColour(blip, COLOUR)
+    SetBlipAsShortRange(blip, true)
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentSubstringPlayerName(LABEL)
+    EndTextCommandSetBlipName(blip)
+end)
---
+++ resources/[standalone]/rme-burgerblip/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'RME'
+description 'Map blip for Giant Burger / Burger Shot MLO (Little Seoul)'
+version '1.0.0'
+
+client_script 'client.lua'
