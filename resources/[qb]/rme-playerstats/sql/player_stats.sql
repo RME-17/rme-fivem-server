8ab30431f157e4641d9220ecc6c270ef577100b0
MR AS GORDON <inforecruitmyenglish@gmail.com>
2026-07-02 12:59:17 +0200

    Fix: add ars_hunting as normal files

--- resources/[qb]/rme-playerstats/sql/player_stats.sql
+++
@@ -1,7 +0,0 @@
--- Run automatically on resource start, but here for reference / manual import.
-CREATE TABLE IF NOT EXISTS `player_stats` (
-    `citizenid` VARCHAR(60) NOT NULL,
-    `stats` LONGTEXT,
-    `updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`citizenid`)
-);
--- resources/npwd_bankingapp/web/dist/__federation_shared_react-4c3735b8.js
+++
@@ -1,4 +0,0 @@
-import { i as f } from "./index-2f456652.js";
-export {
-  f as default
-};
